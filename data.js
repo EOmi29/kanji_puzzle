29840636a6d96411cb43c4fb1ec55d3074f8d482
kyozaidukuru <konakawa-atsushi@higashiomi.ed.jp>
2026-03-13 11:43:33 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 // data.js
 const kanjiData = [
+    // --- 1年生・1学期 ---
     { kanji: "一", grade: 1, term: 1, reading: "いち", words: ["一つ", "一人"] },
     { kanji: "森", grade: 1, term: 2, reading: "もり", words: ["森林", "森の中"] },
     { kanji: "海", grade: 2, term: 1, reading: "うみ", words: ["海岸", "海水"] },
