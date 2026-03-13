aa191849c03db7c1f1d3e7eedf353ea8ef61bf8f
kyozaidukuru <konakawa-atsushi@higashiomi.ed.jp>
2026-03-13 11:38:28 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+// data.js
+const kanjiData = [
+    { kanji: "一", grade: 1, term: 1, reading: "いち", words: ["一つ", "一人"] },
+    { kanji: "森", grade: 1, term: 2, reading: "もり", words: ["森林", "森の中"] },
+    { kanji: "海", grade: 2, term: 1, reading: "うみ", words: ["海岸", "海水"] },
+    // ここに1000文字以上追加しても script.js は汚れません
+];
